21210df72f580c22bbbee2a4136872515cf9e2b6
Michael Robinson <mike@pagesofinterest.net>
2013-10-30 00:37:40 +1300

    added puppet

---
+++ puppet/manifests/main.pp
@@ -0,0 +1,18 @@
+Exec { path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/" ] }
+File { owner => 0, group => 0, mode => 0644 }
+
+stage { 'first': }
+stage { 'last': }
+
+Stage['first'] -> Stage['main'] -> Stage['last']
+
+import 'basic.pp'
+
+import '../../configuration/apache.pp'
+import '../../configuration/mysql.pp'
+import '../../configuration/php.pp'
+import '../../configuration/phpmyadmin.pp'
+
+class{ 'basic':
+  stage => first
+}
