7073aa3809c151d632b0ff3ad01c72668f078c22
Michael Robinson <mike@pagesofinterest.net>
2014-06-30 11:31:45 +1200

    Update bootstrap.sh

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -5,25 +5,16 @@ PUPPET_DIR='/vagrant/puppet'
 
 # NB: librarian-puppet might need git installed. If it is not already installed
 # in your basebox, this will manually install it at this point using apt or yum
-GIT=/usr/bin/git
-APT_GET=/usr/bin/apt-get
-YUM=/usr/sbin/yum
-if [ ! -x $GIT ]; then
-    if [ -x $YUM ]; then
-        yum -q -y install git
-    elif [ -x $APT_GET ]; then
-        apt-get -q -y install git
-    else
-        echo "No package installer available. You may need to install git manually."
-    fi
-fi
+
+apt-get update
+apt-get upgrade -y
+apt-get -q -y install git
 
 if [ `gem query --local | grep librarian-puppet | wc -l` -eq 0 ]; then
-  gem install librarian-puppet
-  cd $PUPPET_DIR && librarian-puppet install --clean
+ gem install librarian-puppet
+ cd $PUPPET_DIR && librarian-puppet install --clean
 else
-  cd $PUPPET_DIR && librarian-puppet update
+ cd $PUPPET_DIR && librarian-puppet update
 fi
 
 sudo -E puppet apply -vv --modulepath=$PUPPET_DIR/modules/ $PUPPET_DIR/manifests/main.pp
-
